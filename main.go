462837741b074dd60d68f3df67e63ea1f5090dec
jordanx8 <60443094+jordanx8@users.noreply.github.com>
2021-07-07 18:51:23 -0500

    Scrapes espn.com
    - can successfully scrape data from ESPN's top 100 for the NBA Draft
    - prints out data into a JSON file

--- main.go
+++ main.go
@@ -3,28 +3,18 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"os"
 	"strconv"
 
 	"github.com/gocolly/colly"
 )
 
-// type Player struct {
-// 	Rank     int    `json:"rank"`
-// 	Name     string `json:"name"`
-// 	School   string `json:"school"`
-// 	Position string `json:"position"`
-// 	Height   string `json:"height"`
-// 	Weight   string `json:"weight"`
-// }
-
 type Player struct {
-	Rank int    `json:"rank"`
-	Name string `json:"name"`
-	// School   string `json:"school"`
-	// Position string `json:"position"`
-	// Height   string `json:"height"`
-	// Weight   string `json:"weight"`
+	Rank     int    `json:"rank"`
+	Name     string `json:"name"`
+	School   string `json:"school"`
+	Position string `json:"position"`
 }
 
 func main() {
@@ -34,16 +24,16 @@ func main() {
 		colly.AllowedDomains("espn.com", "www.espn.com"))
 
 	collector.OnHTML(".draftTable__row li", func(element *colly.HTMLElement) {
-		playerRank, err := strconv.Atoi(element.ChildText("a"))
+		playerRank, err := strconv.Atoi(element.ChildText("span.draftTable__headline--pick"))
 		if err != nil {
 			fmt.Println("Could not get data-rank")
 		}
 
-		playerText := element.Text
-
 		player := Player{
-			Rank: playerRank,
-			Name: playerText,
+			Rank:     playerRank,
+			Name:     element.ChildText("span.draftTable__headline--player"),
+			School:   element.ChildText("span.draftTable__headline--school"),
+			Position: element.ChildText("span.draftTable__headline--pos"),
 		}
 
 		allPlayers = append(allPlayers, player)
@@ -53,9 +43,26 @@ func main() {
 		fmt.Println("Visiting", request.URL.String())
 	})
 
-	collector.Visit("https://www.espn.com/nba/draft/bestavailable")
+	collector.OnHTML(".pagination a", func(e *colly.HTMLElement) {
+		nextPage := e.Request.AbsoluteURL(e.Attr("href"))
+		collector.Visit(nextPage)
+	})
+
+	collector.Visit("https://www.espn.com/nba/draft/bestavailable/_/position/ovr/page/1")
 
 	enc := json.NewEncoder(os.Stdout)
 	enc.SetIndent("", " ")
 	enc.Encode(allPlayers)
+
+	writeJSON(allPlayers)
+}
+
+func writeJSON(data []Player) {
+	file, err := json.MarshalIndent(data, "", " ")
+	if err != nil {
+		fmt.Println("Unable to create JSON file.")
+		return
+	}
+
+	_ = ioutil.WriteFile("playerdata.json", file, 0644)
 }
